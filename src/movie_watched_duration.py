ccf44064de065fcc174d98b365a0fb86e5432a1f
Aashish <aashish@Aashishs-MacBook-Pro.local>
2018-10-30 00:21:12 +0530

    Optimized movie watched issue

--- src/movie_watched_duration.py
+++ src/movie_watched_duration.py
@@ -7,4 +7,24 @@ def total_watched_time(input_data):
     return len(watched_minutes)
 
 
-print total_watched_time([(0,10), (13,18), (15,23), (21,26)])
+def total_watched_time_v2(input_data):
+    input_data.sort()
+    watched_minutes = 0
+    pointer = 0
+    print "lenght is {}".format(len(input_data))
+    for i in range(len(input_data)):
+        if i == 0:
+            s , e = input_data[0]
+            watched_minutes += (e - s)
+        else:
+            s, e = input_data[i]
+            sold, eold = input_data[i-1]
+            if s >= eold:
+                watched_minutes += (e - s)
+            else:
+                watched_minutes += (e - eold)
+        print watched_minutes
+    return watched_minutes
+
+
+print total_watched_time_v2([(0,10), (13,18), (21,26), (15,23)])
